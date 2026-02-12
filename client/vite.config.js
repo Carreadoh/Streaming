a86d527103dd051285bc0d0c01c110826b1f4296
Facundo <167721267+Carreadoh@users.noreply.github.com>
2026-02-12 13:06:14 -0300

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,8 +3,18 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // Esto es para que Vite ignore las etiquetas de Vidstack en el escaneo inicial
+  
   optimizeDeps: {
     exclude: ['@vidstack/react']
+  },
+
+  build: {
+    rollupOptions: {
+      // Marcamos los plugins de Capacitor como externos para que Vercel no falle al compilar
+      external: [
+        '@capacitor/status-bar',
+        '@capacitor/screen-orientation'
+      ]
+    }
   }
 })
